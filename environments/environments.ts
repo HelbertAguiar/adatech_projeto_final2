f4ce498e8a9d9e1655caaee1903a35d11f70956f
Helbert <helbert.aguiar@hotmail.com>
2025-08-15 17:11:11 -0300

    🎨 feat: adicionar tema Angular Material e atualizar estilos; configurar URL base da API; aprimorar configuração do ESLint; atualizar dependências do pacote

---
+++ environments/environments.ts
@@ -0,0 +1,3 @@
+export const apiconsumo = {
+  baseURL: 'https://fakestoreapi.com',
+};
--- eslint.config.js
+++ eslint.config.js
@@ -8,9 +8,9 @@ module.exports = tseslint.config(
     files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
+      // ...tseslint.configs.recommended,
+      // ...tseslint.configs.stylistic,
+      // ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
@@ -35,8 +35,8 @@ module.exports = tseslint.config(
   {
     files: ['**/*.html'],
     extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
+      // ...angular.configs.templateRecommended,
+      // ...angular.configs.templateAccessibility,
     ],
     rules: {},
   },
