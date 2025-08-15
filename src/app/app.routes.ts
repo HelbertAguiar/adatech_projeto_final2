170c60d6970ed22bc966a216ca92a8132f29affe
Helbert <helbert.aguiar@hotmail.com>
2025-08-15 17:38:04 -0300

    🎨 feat: adicionar suporte a animações do Angular; criar componentes Home e ProductCard; atualizar configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent) },
+//   { path: 'produto/:id', loadComponent: () => import('./pages/').then(c => c.ProductDetailComponent) },
+//   { path: 'login', loadComponent: () => import('./pages/').then(c => c.Component) },
+//   { path: 'carrinho', loadComponent: () => import('./pages/').then(c => c.Component) },
+//   { path: 'admin', loadComponent: () => import('./pages/').then(c => c.Component) },
+  { path: '**', redirectTo: '' },
+];
