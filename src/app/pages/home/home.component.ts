102aed2bf17cc47e0a9b4ea1478cdc3648ff7256
Helbert <helbert.aguiar@hotmail.com>
2025-08-17 18:21:07 -0300

    🎨 feat: atualizar componentes de home e product-card; implementar filtros e responsividade

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,14 @@ import { NavbarComponent } from '@shared/components/navbar/navbar.component'
 import { ProductCardComponent } from '@shared/components/product-card/product-card.component'
 import { ProductService } from "@services/product-service.service"
 import { Product } from '@models/product.model';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, NavbarComponent, ProductCardComponent],
+  imports: [CommonModule, NavbarComponent, ProductCardComponent, FormsModule, MatInputModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Product } from '@models/product.model'
 import { Input } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { MatIconModule } from '@angular/material/icon';
+import { TruncatePipe } from '@pipes/truncate.pipe';
 
 @Component({
   selector: 'app-product-card',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, MatIconModule, TruncatePipe ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
